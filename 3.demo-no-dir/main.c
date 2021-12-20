dc6c7526bf7e2f070934661c110f482e8e332e2d
shenkonghui <shenkh1992@gmail.com>
2021-12-20 16:38:37 +0800

    add demo

---
+++ 3.demo-no-dir/main.c
@@ -0,0 +1,8 @@
+#include "add.h"
+#include <stdio.h>
+
+void main(){
+    int a = 1;
+    int b = 2;
+    printf("a+b=%d", add(a , b));
+}
---
+++ 4.shard_lib/main.c
@@ -0,0 +1,10 @@
+#include "add.h"
+#include <stdio.h>
+
+void main()
+{
+    int a = 1;
+    int b = 2;
+    int c = add(a, b);
+    printf("a+b=%d", c);
+}
